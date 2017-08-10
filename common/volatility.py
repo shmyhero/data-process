381fd9446cb20c56b826c27ff2fbec112f04d09c
Elwin Luo <elwin.luo@tradehero.mobi>
2017-08-10 17:59:44 +0800

    volitility

--- common/volatility.py
+++ common/volatility.py
@@ -1,5 +1,7 @@
 import math
 import numpy as np
+import vollib.black_scholes
+import vollib.black_scholes.implied_volatility
 
 
 class Volatility(object):
@@ -11,5 +13,26 @@ class Volatility(object):
             X.append(math.log(price_list[i+1]/price_list[i]))
         return np.std(X)
 
+    @staticmethod
+    def get_year_history_volatility(price_list):
+        return math.sqrt(252) * Volatility.get_history_volatility(price_list)
+
+    @staticmethod
+    def get_black_scholes_option_price(underlying_price, strike_price, days_to_experiation, risk_free_interest_rate, sigma, flag='c'):
+        price = vollib.black_scholes.black_scholes(flag, underlying_price, strike_price, days_to_experiation, risk_free_interest_rate, sigma)
+        return price
+
+
+
 if __name__ == "__main__":
-    print Volatility.get_history_volatility([1.1, 1.2, 1.2, 1.2, 1, 0.9])
+    price_list = [2.682, 2.724, 2.728, 2.689, 2.671, 2.679, 2.682, 2.644, 2.655]
+    print Volatility.get_history_volatility(price_list)
+    sigma = Volatility.get_year_history_volatility(price_list)
+    print sigma
+    sigma = 0.1358
+    bs_price = Volatility.get_black_scholes_option_price(2.655, 2.7, 48/365.0, 0.032, sigma, 'p')
+    print bs_price
+    price = 0.071
+    iv = vollib.black_scholes.implied_volatility.implied_volatility(price, 2.655, 2.7, 48/365.0, 0.032, 'p')
+    print iv
+
