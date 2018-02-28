7b1fead6e23731f8609ff7d01d83eb174e3fa2be
Elwin Luo <elwin.luo@tradehero.mobi>
2018-02-28 17:42:31 +0800

    add time out and retry for http request

--- utils/httphelper.py
+++ utils/httphelper.py
@@ -7,13 +7,21 @@ class HttpHelper:
         pass
 
     @staticmethod
-    def http_get(url, headers=None):
+    def http_get_with_time_out(url, headers=None):
         if headers:
-            f = urllib2.urlopen(urllib2.Request(url=url, headers=headers))
+            f = urllib2.urlopen(urllib2.Request(url=url, headers=headers), timeout=6)
         else:
-            f = urllib2.urlopen(url)
+            f = urllib2.urlopen(url, timeout=6)
         s = f.read()
         return s
 
-
+    @staticmethod
+    def http_get(url, headers=None, retry=3):
+        for i in range(retry):
+            try:
+                content = HttpHelper.http_get_with_time_out(url, headers)
+                return content
+            except Exception:
+                pass
+        raise Exception('Http request failed for %s times' % retry)
 
