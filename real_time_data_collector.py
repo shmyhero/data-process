79515821896b3e7e6ee40b0f5ca552765fe48da6
Elwin Luo <elwin.luo@tradehero.mobi>
2018-05-02 18:04:22 +0800

    update realtime data collector

--- real_time_data_collector.py
+++ real_time_data_collector.py
@@ -13,31 +13,31 @@ from dataaccess.equityrealtimedao import EquityRealTimeDAO
 class RealTimeDataCollector(object):
 
     def __init__(self):
-        self.symbol = 'SVXY'  # , 'VIX', 'SVXY', 'UVXY']
+        self.symbols = ['SVXY', 'SPY']  # , 'VIX', 'SVXY', 'UVXY']
         self.equity_realtime_dao = EquityRealTimeDAO()
 
     @property
     def logger(self):
         return LoggerFactory.create_daily_logger(__name__, PathMgr.get_log_path('realtime'))
 
-    def collect_data(self):
-        self.logger.info('ingest data for %s...' % self.symbol)
+    def collect_data(self, symbol):
+        self.logger.info('ingest data for %s...' % symbol)
         us_dt = datetime.datetime.now(pytz.timezone('US/Eastern'))
         if us_dt.hour == 9 and us_dt.minute == 30 and us_dt.second == 0:
             us_dt = datetime.datetime(us_dt.year, us_dt.month, us_dt.day, 9, 30, 1)
         price = None
         try:
-            price = YahooScraper().get_current_data(self.symbol)
+            price = YahooScraper().get_current_data(symbol)
         except Exception as e:
             self.logger.exception(str(e))
         if price is None:
             try:
-                price = MarketWatchScraper().get_current_data(self.symbol)
+                price = MarketWatchScraper().get_current_data(symbol)
             except Exception as e:
                 self.logger.exception(str(e))
         if price is not None:
             self.logger.info('save record into database...')
-            self.equity_realtime_dao.insert(self.symbol, us_dt, price)
+            self.equity_realtime_dao.insert(symbol, us_dt, price)
         else:
             self.logger.info('ingest record failed at %s...' % us_dt)
 
@@ -46,11 +46,12 @@ class RealTimeDataCollector(object):
         while True:
             if TradeTime.is_market_open():
                 try:
-                    self.collect_data()
-                    self.logger.info('Check missing data...')
-                    count = self.equity_realtime_dao.add_missing_data_in_real_time()
-                    if count is not None and count > 0:
-                        self.logger.info('Filled {} records...'.format(count))
+                    for symbol in self.symbols:
+                        self.collect_data(symbol)
+                        self.logger.info('Check missing data...')
+                        count = self.equity_realtime_dao.add_missing_data_in_real_time(symbol)
+                        if count is not None and count > 0:
+                            self.logger.info('Filled {} records...'.format(count))
                 except Exception as e:
                     self.logger.error('Trace: ' + traceback.format_exc())
                     self.logger.error(str(e))
