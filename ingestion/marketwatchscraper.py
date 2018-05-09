f3fcf79e3cc74b08ba040f4a722a346f464642a6
Elwin Luo <elwinluo@hotmail.com>
2018-05-09 20:14:35 +0800

    update marketwatchscraper

--- ingestion/marketwatchscraper.py
+++ ingestion/marketwatchscraper.py
@@ -4,6 +4,7 @@ from utils.stringhelper import string_fetch
 
 class MarketWatchScraper():
 
+	@staticmethod
     def get_data_by_symbol(symbol):
         url = 'https://www.marketwatch.com/investing/fund/%s' % symbol
         content = HttpHelper.http_get(url)
