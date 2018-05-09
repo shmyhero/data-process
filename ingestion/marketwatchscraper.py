15c5b2807351f77358dc2ea7728b7f5e06bd1d6b
Elwin Luo <elwin.luo@tradehero.mobi>
2018-05-09 15:30:44 +0800

    add missing file of market watch scraper

---
+++ ingestion/marketwatchscraper.py
@@ -0,0 +1,12 @@
+from utils.httphelper import HttpHelper
+from utils.stringhelper import string_fetch
+
+
+class MarketWatchScraper():
+
+    def get_data_by_symbol(symbol):
+        url = 'https://www.marketwatch.com/investing/fund/%s' % symbol
+        content = HttpHelper.http_get(url)
+        content = string_fetch(content, 'mw-rangeBar precision=', 'Day Low')
+        value = string_fetch(content, '\"last-value\">', '</span>')
+        return float(value.replace(',', ''))
