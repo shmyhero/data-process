7a25a2c827d620a4212057058fec161bd0d9519d
Elwin Luo <elwin.luo@tradehero.mobi>
2018-06-05 16:39:40 +0800

    update indicator of rsi and sar

--- utils/indicator.py
+++ utils/indicator.py
@@ -50,75 +50,138 @@ class MACD(object):
             [ema_short, ema_long, dif, dea, bar] = record
         return result
 
+# import talib
+# import numpy
+
 
 class RSI(object):
 
     @staticmethod
-    def get_rsi(price_list):
-        if price_list < 2:
-            return 50
-        positive_sum = 0
-        negative_sum = 0
-        for i in range(len(price_list)-1):
+    def get_init_rsi(price_list):
+        count = len(price_list)-1
+        up_sum = 0
+        down_sum = 0
+        for i in range(count):
             delta = price_list[i+1] - price_list[i]
             if delta > 0:
-                positive_sum += delta
+                up_sum += delta
             else:
-                negative_sum -= delta
-        if positive_sum == negative_sum:
-            return 50
+                down_sum -= delta
+        up_ave = up_sum*1.0 / count
+        down_ave = down_sum * 1.0 / count
+        rsi = 100.0*up_sum/(up_sum + down_sum)
+        return up_ave, down_ave, rsi
+
+    @staticmethod
+    def calc_new_rsi(time_period, delta, rsi_obj):
+        if delta > 0:
+            up = delta*1.0
+            down = 0
         else:
-            return 100.0*positive_sum/(positive_sum + negative_sum)
+            up = 0
+            down = -delta*1.0
+        previous_up, previous_down, previous_rsi = rsi_obj
+        new_up = up/time_period + previous_up*(time_period-1)/time_period
+        new_down = down/time_period + previous_down * (time_period - 1) / time_period
+        rsi = 100 * new_up/(new_up + new_down)
+        return new_up, new_down, rsi
+
+    @staticmethod
+    def get_new_rsi(time_period, price, previous_price, rsi_object=None):
+        delta = price-previous_price
+        return RSI.calc_new_rsi(time_period, delta, rsi_object)
+
+    @staticmethod
+    def get_rsi(price_list, time_period):
+        count = len(price_list)-1
+        if count < 1 or count < time_period:
+            return [None] * (count+1)
+        else:
+            rsi_list = [None] * time_period
+            rsi_object = RSI.get_init_rsi(price_list[0:time_period+1])
+            rsi_list.append(rsi_object[-1])
+            previous_price = price_list[time_period]
+            rest_price_list = price_list[time_period+1:]
+            for price in rest_price_list:
+                rsi_object = RSI.get_new_rsi(time_period, price, previous_price, rsi_object)
+                previous_price = price
+                rsi_list.append(rsi_object[-1])
+            return rsi_list
+
+
+    # @staticmethod
+    # def get_rsi2(price_list, time_period):
+    #     rsi = talib.RSI(numpy.array(price_list), time_period)
+    #     # print rsi
+    #     return rsi
 
 
 class SAR(object):
     """
     psar Parabolic SAR(stop and reverse))
-    psar: (last_high, last_low, bull_p, ep, af, sar)
+    psar: (high_list, low_list, bull_p, af, sar)
     af: acceleration factor
     ep: extreme point
+    iaf: increase affcleration facrtor
     """
     @staticmethod
-    def get_new_sar(high, low, last_psar=None, af0=0.02, iaf=0.02, max_af=0.2):
+    def get_new_sar(high, low, last_psar=None, bar_count=4, af0=0.02, iaf=0.02, max_af=0.2):
         if last_psar is None:
-            return high, low, True, low, af0, low
+            return [high], [low], True, iaf, low
         else:
-            (last_high, last_low, bull_p, ep, af, sar) = last_psar
-        if bull_p:
-            new_sar = sar + af * (ep - sar)
-            if low < new_sar:
-                return high, low, False, low, af0, high
+            (high_list, low_list, bull_p, af, sar) = last_psar
+            last_high = max(high_list)
+            last_low = min(low_list)
+            if len(high_list) >= bar_count:
+                new_high_list = high_list[1:] + [high]
+                new_low_list = low_list[1:] + [low]
             else:
-                if high > ep:
-                    ep = high
-                    af = min(af + iaf, max_af)
-                if last_low < new_sar:
-                    new_sar = last_low
-                return high, low, bull_p, ep, af, new_sar
-        else:
-            new_sar = sar + af * (ep - sar)
-            if high > new_sar:
-                return high, low, True, high, af0, low
+                new_high_list = high_list + [high]
+                new_low_list = low_list + [low]
+                return new_high_list, new_low_list, True, af0, min(new_low_list)
+            new_high = max(new_high_list)
+            new_low = min(new_low_list)
+            if bull_p:
+                ep = last_high
+                new_sar = sar + af * (ep - sar)
+                if new_low < new_sar:
+                    return new_high_list, new_low_list, False, af0, new_high
+                else:
+                    if new_high > last_high:
+                        af = min(af + iaf, max_af)
+                    # if last_low < new_sar:
+                    #     new_sar = last_low
+                    return new_high_list, new_low_list, bull_p, af, new_sar
             else:
-                if low < ep:
-                    ep = low
-                    af = min(af + iaf, max_af)
-                if last_high > new_sar:
-                    new_sar = last_high
-                return high, low, bull_p, ep, af, new_sar
+                ep = last_low
+                new_sar = sar + af * (ep - sar)
+                if new_high > new_sar:
+                    return new_high_list, new_low_list, True, af0, new_low
+                else:
+                    if new_low < last_low:
+                        af = min(af + iaf, max_af)
+                    # if last_high > new_sar:
+                    #     new_sar = last_high
+                    return new_high_list, new_low_list, bull_p, af, new_sar
 
     @staticmethod
-    def get_all_sar(high_pirce_list, low_price_list, af0=0.02, iaf=0.02, max_af=0.2):
+    def get_initial_sar(high_pirce_list, low_price_list, bar_count=4, af0=0.02, iaf=0.02, max_af=0.2):
         psar_list = []
         psar = None
         for i in range(len(high_pirce_list)):
             high = high_pirce_list[i]
             low = low_price_list[i]
-            psar = SAR.get_new_sar(high, low, psar, af0, iaf, max_af)
+            psar = SAR.get_new_sar(high, low, psar, bar_count, af0, iaf, max_af)
             psar_list.append(psar)
-        return map(lambda x: [x[2], x[-1]], psar_list)
+        return psar_list
 
-
+    @staticmethod
+    def get_all_sar(high_pirce_list, low_price_list, bar_count=4, af0=0.02, iaf=0.02, max_af=0.2):
+        psar_list = SAR.get_initial_sar(high_pirce_list, low_price_list, bar_count, af0, iaf, max_af)
+        bull_sar = map(lambda x: [x[2], x[-1]], psar_list)
+        for i in range(bar_count-1):
+            bull_sar[i] = [None, None]
+        return bull_sar
 
 
 
@@ -138,3 +201,5 @@ if __name__ == '__main__':
     print psar
     psar = SAR.get_new_sar(50.08, 50.08, psar)
     print psar
+    print SAR.get_all_sar([31.1, 34.21, 37.63, 41.39, 45.53, 50.08, 55.09],
+                          [25.92, 34.21, 37.63, 41.39, 45.53, 50.08, 55.09])
